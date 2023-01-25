6766d5cc63d10ee6773752089748013ca683e0c3
Marcos Oliveira <oliveira.marcos@engenharia.ufjf.br>
2023-01-25 19:50:18 -0300

    Create User model and CustomUserManager()

--- djangojwtapp/models.py
+++ djangojwtapp/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from managers import CustomUserManager
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = None
+
+    name = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(max_length=100, unique=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    class Meta:
+        app_label = 'djangojwtapp'
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+    def __str__(self):
+        return self.email
