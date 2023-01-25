b7acc32528c4deb6985f685cf00a193a8825d67f
Marcos Oliveira <oliveira.marcos@engenharia.ufjf.br>
2023-01-25 20:02:15 -0300

    Settings, migration and admin

--- djangojwtapp/models.py
+++ djangojwtapp/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from managers import CustomUserManager
+from djangojwtapp.managers import CustomUserManager
 
 
 class User(AbstractUser):
