86cf9515ce03342aa2c522d874d91c014cf7503a
Marcos Oliveira <oliveira.marcos@engenharia.ufjf.br>
2023-01-26 20:08:01 -0300

    Create register view

--- djangojwt/settings.py
+++ djangojwt/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'djangojwtapp'
+    'djangojwtapp',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
--- djangojwtapp/views.py
+++ djangojwtapp/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from djangojwtapp.serializers import UserSerializer
+from rest_framework.response import Response
 
-# Create your views here.
+
+class RegisterView(APIView):
+    @staticmethod
+    def post(request):
+        user = UserSerializer(data=request.data)
+        user.is_valid(raise_exception=True)
+        user.save()
+
+        return Response(user.data)
